43098a5e29207517852c920e1775adc9324a5e0b
hendrimardani <hendrimardani290301@gmail.com>
2026-04-07 03:58:26 +0700

    first commit

--- Project/train.py
+++ Project/train.py
@@ -19,7 +19,7 @@ import mlflow.sklearn
 def eval_metrics(actual, pred):
     rmse = np.sqrt(mean_squared_error(actual, pred))
     mae = mean_absolute_error(actual, pred)
-    r2 = r2_score(actual, pred
+    r2 = r2_score(actual, pred)
     return rmse, mae, r2
 
 
